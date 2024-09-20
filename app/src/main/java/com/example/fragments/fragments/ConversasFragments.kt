1a7bd58ae232706929fa8d9321f3a20d5a4f5ebd
Janaina Tavares <52297249+jana-dev@users.noreply.github.com>
2024-09-20 18:22:13 -0300

    Initial commit

---
+++ app/src/main/java/com/example/fragments/fragments/ConversasFragments.kt
@@ -0,0 +1,23 @@
+package com.example.fragments.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragments.R
+
+class ConversasFragments: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.fragment_conversas, //qual layout de fragment vou utilizar
+            container, //onde ele vai ser colocado (dentro de activiy main colocamos o fragmentContainerView, dentro ele desejamos ter o fragment conversas
+            false
+        )
+    }
+}
